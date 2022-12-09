3a73374b598c9e81f9be0aea989248167b2d8481
elismar <infotec.elismar@gmail.com>
2022-12-09 20:25:46 -0300

    api fake com um arquivo js

---
+++ fake-api/produto.js
@@ -0,0 +1,17 @@
+[
+    {"nome":"Letra A","img":"https://elismarrodrigues22.github.io/proj-loja-01/fake-api/imagens/letraA.jpg","valor":25.0},
+    {"nome":"Letra B","img":"https://elismarrodrigues22.github.io/proj-loja-01/fake-api/imagens/letraB.jpg","valor":25.0},
+    {"nome":"Letra C","img":"https://elismarrodrigues22.github.io/proj-loja-01/fake-api/imagens/letraC.jpg","valor":25.0},
+    {"nome":"Letra D","img":"https://elismarrodrigues22.github.io/proj-loja-01/fake-api/imagens/letraD.jpg","valor":25.0},
+    {"nome":"Letra E ","img":"https://elismarrodrigues22.github.io/proj-loja-01/fake-api/imagens/letraE.jpg","valor":25.0},
+    {"nome":"Letra F","img":"https://elismarrodrigues22.github.io/proj-loja-01/fake-api/imagens/letraF.jpg","valor":25.0}
+]
+
+
+letraA.jpg
+letraB.jpg
+letraC.jpg
+letraD.jpg
+letraE.jpg
+letraF.jpg
+
